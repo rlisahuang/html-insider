f10403756c1cdf1f315a6d691081c79383ba4d31
Ruanqianqian (Lisa) Huang <rlisahuang@gmail.com>
2022-04-24 18:52:35 -0700

    got debugger api to work; next to figure out how to set breakpoitns

--- index.ts
+++ index.ts
@@ -4,6 +4,12 @@ import * as chromeLauncher from "chrome-launcher";
 type LaunchedChrome = chromeLauncher.LaunchedChrome;
 
 const dest = "file:///Users/lisa/projects/LiveWeb/tasks/toy/test.html";
+const program = `
+    document.querySelector('button#btn1');
+    // btn.click();
+    // document.body.innerHTML;
+    // getEventListeners(btn);
+`;
 
 // Optional: set logging level of launcher to see its output.
 // Install it using: npm i --save lighthouse-logger
@@ -17,7 +23,7 @@ const dest = "file:///Users/lisa/projects/LiveWeb/tasks/toy/test.html";
  * @return {Promise<LaunchedChrome>}
  */
 function launchChrome(headless: boolean | undefined = true): Promise<LaunchedChrome> {
-//   headless = false;
+  headless = false;
   return chromeLauncher.launch({
     port: 9224, // Uncomment to force a specific port of your choice.
     chromeFlags: [
@@ -31,26 +37,47 @@ function launchChrome(headless: boolean | undefined = true): Promise<LaunchedChr
 
 async function main() {
   const chrome = await launchChrome();
-  const protocol = await CDP({ port: chrome.port });
+  const protocol = await CDP({ port: chrome.port }); // client type
+
 
   // Extract the DevTools protocol domains we need and enable them.
   // See API docs: https://chromedevtools.github.io/devtools-protocol/
-  const { Page, Runtime } = protocol;
+  const { Page, Runtime, Debugger, DOMDebugger } = protocol;
   await Promise.all([Page.enable(), Runtime.enable()]);
 
   Page.navigate({ url: dest });
 
+
   // Wait for window.onload before doing stuff.
   Page.on('loadEventFired', async () => {
-    const js = "document.querySelector('button#btn1').innerHTML";
-    // Evaluate the JS expression in the page.
-    const result = await Runtime.evaluate({ expression: js });
+    // chromeDebugger.
+    const js = program;
+    
+    const result = await Runtime.evaluate({ expression: "document.querySelector('button#btn1')" });
+    const btnID = result.result.objectId;
 
-    console.log(result);
+    const listeners = (await DOMDebugger.getEventListeners({ objectId: btnID })).listeners;
+
+    console.log(listeners);
+    // TODO: figure out how to set and pause and remove breakpoints
+    
+    await DOMDebugger.setEventListenerBreakpoint({ eventName: 'click' });
+    // Debugger.on('paused', () => {
+    //     console.log('stopped');
+    //     Debugger.resume({});
+    // });
+
+    await Runtime.evaluate({ expression: "document.querySelector('button#btn1').click()" });
+
+
+
+    // console.log(result);
     // console.log("Title of page: " + result.result.value);
 
+    setTimeout(() => {
     protocol.close();
     chrome.kill(); // Kill Chrome.
+    }, 60000);
   });
 }
 
